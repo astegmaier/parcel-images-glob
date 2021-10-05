2a3f5a2ad7738b4c8aef8730efbfdb00d33877ae
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-10-05 17:52:35 -0400

    Render image urls using glob.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import images from "url:./images/*.jpg";
 
-const App = () => {
-  return <div>Hello World</div>
-};
+const App = () => (
+    <div>
+      <h1>Images Imported with Glob Import</h1>
+      {Object.entries(images).map(([imageName, imageUrl]) => (
+        <div>
+          <h2>{imageName}</h2>
+          <img src={imageUrl} width={300} />
+        </div>
+      ))}
+    </div>
+  );
+
 
 export default App;
